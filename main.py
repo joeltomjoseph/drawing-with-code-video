e86401ba7dbc8a4f31830b5d386d3b248a28dcc8
probablynotjoel <joeltom1408@gmail.com>
2020-07-09 17:30:53 +0100

    Finished code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 
+'''This is the spiral design'''
 colors = [ "red","purple","blue","green","orange","yellow"]
 my_pen = turtle.Pen()
 turtle.bgcolor("black")
@@ -12,3 +13,34 @@ for x in range(3600):
    my_pen.left(59)
 turtle.update()
 turtle.mainloop()
+
+'''And this is the smiley face'''
+#wn = turtle.Screen()
+#smiles = turtle.Turtle()
+#smiles.fillcolor("red")
+#
+#smiles.begin_fill()
+#smiles.penup()
+#smiles.goto(-75,150)
+#smiles.pendown()
+#smiles.circle(10)
+#smiles.end_fill()     
+#
+#smiles.begin_fill()
+#smiles.penup()
+#smiles.goto(75,150)
+#smiles.pendown()
+#smiles.circle(10)
+#smiles.end_fill()
+#
+#smiles.penup()
+#smiles.goto(0,0)
+#smiles.pendown()
+#smiles.circle(100,90)
+#
+#smiles.penup()           
+#smiles.setheading(180)
+#smiles.goto(0,0)
+#smiles.pendown()
+#smiles.circle(-100,90)
+#turtle.mainloop()
